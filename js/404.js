59308ae5f05249eeda3c436969b79eb29e0157d6
Anthony Mejia <anthony.mejia1194@gmail.com>
2016-07-06 20:24:39 -0400

    updated 404 parallax scene

--- js/404.js
+++ js/404.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-	
     $('#scene').parallax({
         calibrateX: true,      
         calibrateY: true,
         invertX: false,
         invertY: true,
-        limitX: false,
-        limitY: false,
+        limitX: 80,
+        limitY: 30,
         scalarX: 10,
         scalarY: 35,
         frictionX: 0.2,
-        frictionY: 0.8,
+        frictionY: 0.6,
         originX: 0.0,
         originY: 1.0
     });
